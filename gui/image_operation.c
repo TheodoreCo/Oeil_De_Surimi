9d446b85fea7a73cd913e6e4ab0da5952d985ac7
TheodoreCo <theodore.coman@gmail.com>
2018-10-13 21:16:37 +0200

    Refactor main.c and define the binary_image struct
    GUI callback functions moved from main.c to img_treatment.c
    New Codeblock target to handle the XOR test

--- gui/image_operation.c
+++ gui/image_operation.c
@@ -1,8 +1,12 @@
 #include <gtk/gtk.h>
 #include <stdlib.h>
+#include "../image_treatment/img_treatment.h"
+#include "../neural_network/neural_network.h"
 
-extern unsigned char **datax;
-extern int img_h, img_w;
+enum Bin_Img_Type {DO_NOTHING, GRAYSCALE, B_AND_W, B_AND_W_DENOISED} bin_img_type;
+
+extern GtkBuilder *builder; /* Used to get widgets from different functions */
+extern binary_image *b_image;
 
 // TODO: checks: is the bitmap file really a bitmap ? If so, is it a 24 bitmap ? Without compression, etc ?
 
@@ -23,80 +27,169 @@ gboolean draw_picture(GtkWidget *widget, cairo_t *cr, gpointer data)
     return FALSE;
 }
 
-//void img_bmp_info(char *filename, unsigned int *file_len, int *dib_header_len)
-//{
-//    FILE *file = fopen(filename, "rb");
-//    if(file)
-//    {
-//        // Position 2: the file length (little-endian format)
-//        fseek(file, 2L, SEEK_SET);
-//        fread(file_len, 4, 1, file);
-//
-//        fseek(file, 14L, SEEK_SET);
-//        fread(dib_header_len, 4, 1, file);
-//        fread(&img_w, 4, 1, file);
-//        fread(&img_h, 4, 1, file);
-//
-//        fclose(file);
-//    }
-//}
-
-void img_bmp_to_binary(char *filename)
+void on_oeil_de_surimi_select_img_clicked(GtkButton *button, GtkImage *image)
 {
-    // const int NUM_PIXEL_BYTES = 3; /* Number of bytes per pixel. We only handle 24 bits bitmaps for now. */
-    const int BMP_FILE_HEADER_LEN = 14;
+    GtkEntry *file_gtk_entry = GTK_ENTRY(gtk_builder_get_object(builder, "oeil_de_surimi_bmp_file_name"));
+    guint16 file_name_len = gtk_entry_get_text_length(file_gtk_entry);
+    printf("Displayed file name length = '%d'\n", file_name_len);
+    gchar *file_name = NULL;
+    GtkWidget *dialog = NULL;
 
-    FILE *file = fopen(filename, "rb");
-    if(file)
+    if(file_name_len == 0)
     {
-        unsigned int file_len;
-        unsigned int dib_header_len;
+        // Get the parent window for the dialog
+        GtkWindow *parent_window = GTK_WINDOW(gtk_builder_get_object(builder, "oeil_de_surimi_main_ocr"));
 
-        // Position 2: the file length (little-endian format)
-        fseek(file, 2L, SEEK_SET);
-        fread(&file_len, 4, 1, file);
+        dialog = gtk_file_chooser_dialog_new(
+                      "Open File",
+                      parent_window,
+                      GTK_FILE_CHOOSER_ACTION_OPEN,
+                      ("Cancel"), GTK_RESPONSE_CANCEL,
+                      ("Open"), GTK_RESPONSE_ACCEPT,
+                      NULL);
 
-        fseek(file, 14L, SEEK_SET);
-        fread(&dib_header_len, 4, 1, file);
-        fread(&img_w, 4, 1, file);
-        fread(&img_h, 4, 1, file);
-
-        printf("File len: %d; DIB header len: %d; Image width: %d; Image height: %d\n",
-               file_len, dib_header_len, img_w, img_h);
-
-        datax= malloc(img_h*sizeof(unsigned char *));
-        for(int i = 0; i < img_h; i++)
-            datax[i] = malloc(sizeof(unsigned char) * img_w);
-        /*
-        Is there some padding ? "The bits representing the bitmap pixels are packed in rows.
-        The size of each row is rounded up to a multiple of 4 bytes (a 32-bit DWORD) by padding." (wiki)
-        */
-        int padding = img_w % 4;
-
-        fseek(file, BMP_FILE_HEADER_LEN + dib_header_len, SEEK_SET);
-        /*
-        "Usually pixels are stored "bottom-up", starting in the lower left corner,
-        going from left to right, and then row by row from the bottom to the top of the image." (wiki)
-        ==> img_h descending loop, img_w ascending loop.
-        */
-        for(int i = img_h-1; i >= 0; i--)
+        if (gtk_dialog_run (GTK_DIALOG (dialog)) == GTK_RESPONSE_ACCEPT)
         {
-            for(int j = 0; j < img_w; j++)
-            {
-                unsigned char r, g, b;
-                fread(&r, sizeof(unsigned char), 1, file);
-                fread(&g, sizeof(unsigned char), 1, file);
-                fread(&b, sizeof(unsigned char), 1, file);
-
-                datax[i][j] = ((int)r + (int)g + (int)b) / 3;
-            }
-
-            if(padding != 0)
-            {
-                fseek(file, padding, SEEK_CUR);
-            }
-            // printf("Reached position:  %d\n", (int)ftell(file));
+            file_name = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (dialog));
+        } else {
+            return;
         }
-        fclose(file);
+
+        printf("Chosen file name:%s\n", file_name);
+        gtk_entry_set_text(file_gtk_entry, file_name);
+    } else {
+        // We already have the file name: no need to open file chooser dialog
+        file_name = malloc(file_name_len * sizeof (gchar));
+        strcpy(file_name, gtk_entry_get_text(file_gtk_entry));
     }
+
+    GError *error = NULL;
+    GdkPixbuf *pix = gdk_pixbuf_new_from_file (file_name, &error);
+    if (pix == NULL)
+    {
+        g_printerr ("Error loading file: #%d %s\n", error->code, error->message);
+        g_error_free (error);
+        return;
+    }
+    else
+    {
+        gtk_image_clear(image);
+        g_signal_connect(image, "draw", G_CALLBACK(draw_picture), pix);
+    }
+
+    b_image = bi_image_from_file(file_name);
+
+    g_free (file_name);
+
+    if(dialog)
+    {
+        gtk_widget_destroy (dialog);
+    }
+}
+
+gboolean draw_callback(GtkWidget *widget, cairo_t *cr, gpointer data)
+{
+    if(b_image && bin_img_type != DO_NOTHING)
+    {
+        guint width,height;
+
+        width   = gtk_widget_get_allocated_width(widget);
+        height  = gtk_widget_get_allocated_height(widget);
+
+        gtk_widget_set_size_request (widget, width, height);
+
+        cairo_set_line_width (cr, 1.0);
+        cairo_set_line_cap (cr, CAIRO_LINE_CAP_ROUND);
+
+        int img_w = b_image->w;
+        int img_h = b_image->h;
+        for(int y=0; y<img_h; y++)
+        {
+            for(int x=0; x<img_w; x++)
+            {
+                double col_pix = b_image->pixel[y*img_w + x]/255.0;
+                if (bin_img_type == B_AND_W)
+                {
+                    col_pix = col_pix > 0.5 ? 1 : 0;
+                }
+                cairo_set_source_rgb (cr, col_pix, col_pix, col_pix);
+//                printf(" %u", col_pix);
+                cairo_move_to(cr, x, y);
+                cairo_line_to(cr, x, y);
+
+                cairo_stroke (cr);
+            }
+        }
+    }
+    return FALSE;
+}
+
+void on_oeil_de_surimi_grayscale_btn_clicked(GtkButton *button, GtkDrawingArea *drawing_area)
+{
+    bin_img_type = GRAYSCALE;
+    gtk_widget_queue_draw(GTK_WIDGET(drawing_area));
+}
+
+void on_oeil_de_surimi_b_and_w_btn_clicked(GtkButton *button, GtkDrawingArea *drawing_area)
+{
+    bin_img_type = B_AND_W;
+    gtk_widget_queue_draw(GTK_WIDGET(drawing_area));
+}
+
+void on_oeil_de_surimi_run_xor_btn_clicked(GtkButton *button) {
+    // Get needed values to build the NN
+    GtkSpinButton *hid_lay_spin_btn = GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "oeil_de_surimi_hid_lay_val"));
+    const double hidden_layers = gtk_spin_button_get_value(hid_lay_spin_btn);
+    GtkSpinButton *hid_neur_spin_btn = GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "oeil_de_surimi_hid_neur_val"));
+    const double hidden_neurons = gtk_spin_button_get_value(hid_neur_spin_btn);
+
+    // Build a NN with 2 input neurons, 1 output neuron and the GUI-specified layers & neurons
+    neur_net *nn = nn_new_instance(2, hidden_layers, hidden_neurons, 1);
+
+    // Get needed values to train the NN
+    GtkSpinButton *epochs_spin_btn = GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "oeil_de_surimi_epochs_val"));
+    const double num_epochs = gtk_spin_button_get_value(epochs_spin_btn);
+    GtkSpinButton *learning_rate_spin_btn = GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "oeil_de_surimi_learning_rate_val"));
+    const double learning_rate = gtk_spin_button_get_value(learning_rate_spin_btn);
+
+    // Train it for XOR
+    nn_xor_learn(nn, num_epochs, learning_rate);
+
+    // Get the widgets to show results
+    GtkEntry *xor_out_1_entry = GTK_ENTRY(gtk_builder_get_object(builder, "oeil_de_surimi_xor_out_1_entry"));
+    GtkEntry *xor_out_2_entry = GTK_ENTRY(gtk_builder_get_object(builder, "oeil_de_surimi_xor_out_2_entry"));
+    GtkEntry *xor_out_3_entry = GTK_ENTRY(gtk_builder_get_object(builder, "oeil_de_surimi_xor_out_3_entry"));
+    GtkEntry *xor_out_4_entry = GTK_ENTRY(gtk_builder_get_object(builder, "oeil_de_surimi_xor_out_4_entry"));
+
+
+    // Run NN 4 times & update GUI to show results
+    double const inputs[4][2] = {
+        {0.0, 0.0},
+        {0.0, 1.0},
+        {1.0, 0.0},
+        {1.0, 1.0}
+    };
+
+
+    gchar *text1 = g_strdup_printf ("%.2f", *nn_run(nn, inputs[0]));
+    gchar *text2 = g_strdup_printf ("%.2f", *nn_run(nn, inputs[1]));
+    gchar *text3 = g_strdup_printf ("%.2f", *nn_run(nn, inputs[2]));
+    gchar *text4 = g_strdup_printf ("%.2f", *nn_run(nn, inputs[3]));
+
+    gtk_entry_set_text(xor_out_1_entry, text1);
+    gtk_entry_set_text(xor_out_2_entry, text2);
+    gtk_entry_set_text(xor_out_3_entry, text3);
+    gtk_entry_set_text(xor_out_4_entry, text4);
+
+    g_free(text1);
+    g_free(text2);
+    g_free(text3);
+    g_free(text4);
+
+    nn_free(nn);
+}
+
+void on_oeil_de_surimi_def_nn_values_btn_clicked(GtkButton *button) {
+    // TODO
+    // Reinitialize default values
 }
--- gui/image_operation.h
+++ gui/image_operation.h
@@ -1,7 +1,22 @@
 #ifndef IMAGE_OPERATION_H_INCLUDED
 #define IMAGE_OPERATION_H_INCLUDED
+
 gboolean draw_picture(GtkWidget *widget, cairo_t *cr, gpointer data);
+
+/* Builds the binary image by calling bi_image_from_file() */
+void on_oeil_de_surimi_select_img_clicked(GtkButton *button, GtkImage *image);
+
+/* Callback function to draw the binary image */
+gboolean draw_callback(GtkWidget *widget, cairo_t *cr, gpointer data);
+
+void on_oeil_de_surimi_grayscale_btn_clicked(GtkButton *button, GtkDrawingArea *drawing_area);
+
+void on_oeil_de_surimi_b_and_w_btn_clicked(GtkButton *button, GtkDrawingArea *drawing_area);
+
+void on_oeil_de_surimi_run_xor_btn_clicked(GtkButton *button);
+
+void on_oeil_de_surimi_def_nn_values_btn_clicked(GtkButton *button);
+
 //void img_bmp_info(char *filename, unsigned int *file_len, int *dib_header_len);
-void img_bmp_to_binary(char *filename);
 
 #endif // IMAGE_OPERATION_H_INCLUDED
