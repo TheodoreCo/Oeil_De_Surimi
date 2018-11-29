e32251043b81b30e1f2eb1d72ccef3f9da1a6df1
Stephane Mabille <stephane.mabille@epita.fr>
2018-11-29 23:44:12 +0100

    gefedetruks
    Signed-off-by: Stephane Mabille <stephane.mabille@epita.fr>

--- neural_network/main.c
+++ neural_network/main.c
@@ -3,12 +3,43 @@
 #include <math.h>
 #include <time.h>
 #include <err.h>
+#include <dirent.h>
 
 #include "neural_network.h"
 
 int main(/* int argc, char *argv[] */)
 {
+	/*
+	// RECUPERATION DES CARACTERES 
 	
+	struct dirent *pDirent;
+	
+	DIR *MajDir;
+	MajDir = opendir("./../Dataset/Majuscules");
+	if (MajDir == NULL)
+		errx(1, "Could not open directory Majuscules, wrong path ?");
+	
+	DIR *MinDir;
+	MinDir = opendir("./../Dataset/Minuscules");
+	if (MinDir == NULL)
+		errx(1,"Could not open directory Minuscules, wrong path ?");
+
+	DIR *NumDir;
+	NumDir = opendir("./../Dataset/Chiffres");
+	if (NumDir == NULL)
+		errx(1,"Could not open directory Chiffres, wrong path ?");
+	
+
+	DIR *PoncDir;
+	PoncDir = opendir("./../Dataset/Ponctuation");
+	if (PoncDir == NULL)
+		errx(1, "Could not open directory Ponctuation, wrong path ?");
+
+
+
+
+
+
 	// INSTIATE NN
 
 	size_t num_inputs = 256;
@@ -17,8 +48,35 @@ int main(/* int argc, char *argv[] */)
 	size_t num_outputs = 93;
 
 	neur_net *nn = instantiate(num_inputs,num_hidden_layers,num_hidd_neur,num_outputs);
+	
+	// TRAINING DES MAJUSCULES
+	FILE *fichier;
 
-	nn_save(nn,"Reseau_test");
+	while((pDirent = readdir(MajDir)) != 0)
+	{
+		char *act = pDirent->d_name;
+		fichier = fopen(act,"r");
+		int num;
+		char **line;
+		while(getline(line,) != -1)
+		{
+			fscanf(fichier,"%d", &num);
+			printf("Value of n=%d\n", num);
+		}
+	}
+	*/
+
+	size_t num_inputs = 256;
+	size_t num_hidden_layers = 2;
+	size_t num_hidd_neur = 3;
+	size_t num_outputs = 93;
+
+	neur_net *nn = instantiate(num_inputs,num_hidden_layers,num_hidd_neur,num_outputs);
+	
+	xor_train(nn, 0.1);
+
+	double *tab
+
 
 	return 0;
 }
